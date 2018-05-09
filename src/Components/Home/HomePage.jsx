d7d6fc1e82893fa792f884a68ca88e2e9d0de710
greg <gregmklein@gmail.com>
2018-05-08 23:47:54 -0400

    Home page and styling

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import './Styles/HomePage.css';
 
 export class HomePage extends Component {
+  constructor(){
+    super();
+    this.hostButtonClick = this.hostButtonClick.bind(this);
+    this.state = {hidden: false};
+  }
+
+  hostButtonClick() {
+    this.setState({hidden: true});
+  }
+
+
   render() {
     return (
-      <div id="homepage">
-        <button>Host</button>
+      <div id="homepage" className={(this.state.hidden ? "loading" : "")}>
+        <div className="homepage-centered">
+          <div className="wrapper">
+            <h1>Welcome to the word game!</h1>
+          </div>
+          <div className="wrapper">
+            <button id="host-button" onClick={this.hostButtonClick}>Host</button>
+          </div>
+        </div>
       </div>
     );
   }
